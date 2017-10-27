01ff1615bd6273ff1f3a3bfff082b2857c33374e
David Martin <david.martin@redhat.com>
2017-10-27 17:19:27 +0100

    Add mem output

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-const express = require('express');
+var lastMem = process.memoryUsage()
+console.log(lastMem);
 
-console.log('app');
+var currentMem = {}
+var memDiff = {}
 
-setTimeout(()=> {
-  console.log('exiting'); 
-}, 20000);
+function compareMem() {
+  currentMem = process.memoryUsage()
+  memDiff.rss = currentMem.rss - lastMem.rss
+  memDiff.heapTotal = currentMem.heapTotal - lastMem.heapTotal
+  memDiff.heapUsed = currentMem.heapUsed - lastMem.heapUsed
+  memDiff.external = currentMem.external - lastMem.external
+  console.log('MEM DIFF', memDiff)
+  lastMem = JSON.parse(JSON.stringify(currentMem))
+}
+
+
+compareMem()
+
+const express = require('express')
+console.log('express required')
+compareMem()
+
+const parseurl = require('parseurl')
+console.log('parseurl required')
+compareMem()
+
+console.log('done')
